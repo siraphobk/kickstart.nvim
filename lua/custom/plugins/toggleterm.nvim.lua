4a01d646873d062241f1371ee9cabfe96ce5d93a
siraphobk <siraphob.k@outlook.com>
2026-04-22 12:22:50 +0700

    Change toggleterm to use vertical direction instead

--- lua/custom/plugins/toggleterm.nvim.lua
+++ lua/custom/plugins/toggleterm.nvim.lua
@@ -1,4 +1,15 @@
-return { 'akinsho/toggleterm.nvim', version = '*', opts = {
-  open_mapping = [[<c-\>]],
-  direction = 'float',
-} }
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    open_mapping = [[<c-\>]],
+    direction = 'vertical',
+    size = function(term)
+      if term.direction == 'horizontal' then
+        return 15
+      elseif term.direction == 'vertical' then
+        return vim.o.columns * 0.3
+      end
+    end,
+  },
+}
