40dfe9abb697ba53e3fd37c9f92816e60f4e1f38
siraphobk <siraphob.k@outlook.com>
2026-06-08 16:19:21 +0700

    feat: Add gruvbox theme

--- init.lua
+++ init.lua
@@ -923,8 +923,9 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    -- The active colorscheme lives in lua/custom/plugins/gruvbox-material.lua.
+    -- tokyonight is kept installed as an alternative (switch via `:Telescope colorscheme`).
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
@@ -934,11 +935,6 @@ require('lazy').setup({
         },
         dim_inactive = true,
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
